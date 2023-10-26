5fc5cfcc2f14bc47684c85999f9995bea47b0aaa
David Vaughan <dvaug86@gmail.com>
2023-10-26 09:46:30 -0400

    created component folder and installed bootstrap

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h3>Todo List</h3>
+      <button>Create Task</button>
+    </div>
+  );
+};
+
+export default TodoList;
