9c8bbfe1ed84094c2cda9e29b32084cb71629d7c
David Vaughan <dvaug86@gmail.com>
2023-11-01 09:42:18 -0400

    created framework for modal from reactstrap

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateTask from "../modals/createTask"
 
 const TodoList = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
   return (
-    <div>
-      <h3>Todo List</h3>
-      <button className="btn btn-primary">Create Task</button>
-    </div>
+    <>
+      <div className="header text-center">
+        <h3>Todo List</h3>
+        <button className="btn btn-primary mt-2" onClick={() => setModal(true)}>Create Task</button>
+      </div>
+      <div className="task-container">
+        stuff
+      </div>
+      <CreateTask toggle={toggle} modal={modal} />s
+    </>
   );
 };
 
