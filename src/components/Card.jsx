685df9f96a650c15901bdd1600ee87f9fe59251d
David Vaughan <dvaug86@gmail.com>
2023-12-05 09:14:25 -0500

    updated styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 
 const Card = ({ taskObj, index }) => {
+  const colors = [
+    {
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
+    },
+    {
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
+    },
+    {
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
+    },
+    {
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1",
+    },
+    {
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD",
+    },
+  ];
+
   return (
-    <div class="card-wrapper mr-5">
+    <div className="card-wrapper me-5">
       <div
-        class="card-top"
-        // style={{ "background-color": colors[index % 5].primaryColor }}
-        style={{ "background-color": "chartreuse" }}
+        className="card-top"
+        style={{ "background-color": colors[index % 5].primaryColor }}
       ></div>
-      <div class="task-holder">
+      <div className="task-holder">
         <span
-          class="card-header"
+          className="card-header"
           style={{
-            // "background-color": colors[index % 5].secondaryColor,
-            "background-color": "F2FAF1",
+            "background-color": colors[index % 5].secondaryColor,
             "border-radius": "10px",
           }}
         >
@@ -23,15 +44,14 @@ const Card = ({ taskObj, index }) => {
 
         <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
           <i
-            class="far fa-edit mr-3"
-            // style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
-            style={{ color: "#5DC250", cursor: "pointer" }}
+            className="far fa-edit me-3"
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
             // onClick={() => setModal(true)}
           ></i>
           <i
-            class="fas fa-trash-alt"
-            // style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
-            style={{ color: "#5DC250", cursor: "pointer" }}
+            className="fas fa-trash-alt"
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+
             // onClick={handleDelete}
           ></i>
         </div>
